3dddd4c93d6696f4fec6e3b9ea31f721f106d064
Lia-Siao <91170844+Lia-Siao@users.noreply.github.com>
2022-04-14 17:46:33 +0800

    add week08

---
+++ week08/week08-1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+char names[20][20];
+int grades[20];
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    char name[20];
+    int grade;
+    int i=0;
+    while(scanf("%s %d",&name,&grade)==2){
+        names[i]<=name
+        gardes[i]<=grade
+        i++;
+    }
+    for(int i=0;i<;i++){
+        printf("%s %d\n",names[i],grades[i]);
+    }
+}
